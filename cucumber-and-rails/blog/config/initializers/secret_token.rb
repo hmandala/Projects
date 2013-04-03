86c9324e0382f88bf7362c3ab92903b4cb80bf1b
Harish Mandala <hmandala@ubuntu.(none)>
2013-04-02 18:05:29 -0700

    reorganized

---
+++ cucumber-and-rails/blog/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '81ca0f394bc0b8688d2bcc7da687183df2ed02fcd7459f0eeb01c0c8a1c23d1eae372e1e79df6eabf2ed16875b4dee2a7e7dcaeb7568cc859ab6c1879c8aaa88'
